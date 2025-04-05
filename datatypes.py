8541998cc47b56d49d3a1db5abef66e5f4ad6b73
Sakshax <sakshamchoudhari03@gmail.com>
2025-04-05 19:47:02 +0530

    Add datatypes.py with descriptions of Python data types

---
+++ datatypes.py
@@ -0,0 +1,24 @@
+#This code decribres about datatypes in python
+
+
+"""
+# Data types in Python
+# Python has several built-in data types that are used to store different types of values.
+# The most commonly used data types are:
+# 1. Numeric Types:             
+#   - int: Integer type, used to store whole numbers.
+#   - float: Floating-point type, used to store decimal numbers.
+#   - complex: Complex number type, used to store complex numbers.
+# 2. Sequence Types:
+#   - str: String type, used to store sequences of characters.
+#   - list: List type, used to store ordered collections of items.
+#   - tuple: Tuple type, used to store ordered collections of items that cannot be changed.
+# 3. Mapping Type:
+#   - dict: Dictionary type, used to store key-value pairs.
+# 4. Set Types:
+#  - set: Set type, used to store unordered collections of unique items.    
+#  - frozenset: Immutable set type, used to store unordered collections of unique items that cannot be changed.
+# 5. Boolean Type:
+#   - bool: Boolean type, used to store True or False values.
+"""
+
