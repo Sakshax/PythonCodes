cc83109d808ac6ad158a996c321a581648ffc27a
Sakshax <sakshamchoudhari03@gmail.com>
2025-04-06 23:34:42 +0530

    modified while-loop.py

---
+++ Iteration statement/while-loop.py
@@ -0,0 +1,16 @@
+# A while loop in Python is used to repeatedly execute a block of code 
+# as long as a given condition is true.
+
+# Example of a while loop:
+count = 0
+
+while count < 5:  # Loop will run as long as count is less than 5
+    print("Count is:", count)
+    count += 1  # Increment count to avoid infinite loop
+
+#output:
+# Count is: 0
+# Count is: 1
+# Count is: 2
+# Count is: 3  
+# Count is: 4
