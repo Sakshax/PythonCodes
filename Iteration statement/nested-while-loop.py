0febbab9539f196eeecacf2fc647c680f3e72e13
Sakshax <sakshamchoudhari03@gmail.com>
2025-04-07 22:56:56 +0530

    Add example of a nested while loop in Python

---
+++ Iteration statement/nested-while-loop.py
@@ -0,0 +1,8 @@
+# Example of a nested while loop
+i = 1
+while i <= 3:  # Outer loop
+    j = 1
+    while j <= 2:  # Inner loop
+        print(f"Outer loop: {i}, Inner loop: {j}")
+        j += 1
+    i += 1
