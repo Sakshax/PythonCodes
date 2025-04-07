3073873aec5c8ea4d40dd7b96a1d919acc869705
Sakshax <sakshamchoudhari03@gmail.com>
2025-04-07 23:03:33 +0530

    Add example of the pass statement in Python

---
+++ Iteration statement/pass(null)-statement.py
@@ -0,0 +1,14 @@
+# Example of pass statement
+
+def my_function():
+    # Function implementation will be added later
+    pass
+
+class MyClass:
+    pass  # Placeholder for an empty class
+
+for i in range(5):
+    if i % 2 == 0:
+        pass  # Placeholder for future code
+    else:
+        print(i)
