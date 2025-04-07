cecea7365e5b2c11bcc7dcb42b7980d5c8a3eafe
Sakshax <sakshamchoudhari03@gmail.com>
2025-04-07 22:54:54 +0530

    Add example of a nested for loop in Python

---
+++ Iteration statement/nested-for-loop.py
@@ -0,0 +1,9 @@
+# Example of a nested for loop in Python
+
+# Outer loop
+for i in range(1, 4):  # Iterates from 1 to 3
+    print(f"Outer loop iteration {i}")
+    
+    # Inner loop
+    for j in range(1, 4):  # Iterates from 1 to 3
+        print(f"  Inner loop iteration {j}")
